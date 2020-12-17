841d26afba4fdeed78e340f13e8cdd10f6efcba8
Andreas Olsson <noname.olsson@gmail.com>
2020-12-17 20:05:18 +0100

    Add some stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,10 @@
 module.exports = {
-  parserOptions: {
-    ecmaVersion: 2019,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
   extends: [
     'react-app',
-    'eslint:recommended',
-    'eslint-config-prettier',
     'plugin:react-perf/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
-  plugins: ['prettier', 'react', 'react-hooks', 'react-perf'],
+  plugins: ['react', 'react-hooks', 'react-perf'],
   settings: {
     react: {
       version: 'detect',
